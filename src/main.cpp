d8889730ed3d0e1bfe17dce1ecae815c96ce3fb0
TheCharlatan <seb.kung@gmail.com>
2025-07-05 16:28:24 +0200

    Support big numbers

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
+#include "arith_uint256.h"
+#include "uint256.h"
+
+#include <gmpxx.h>
+
+#include <boost/multiprecision/cpp_int.hpp>
+using namespace boost::multiprecision;
+
 #include <cassert>
 #include <cstdint>
 #include <cstdio>
 #include <utility>
 
-constexpr uint64_t fast_mod_exp(const uint64_t num, uint64_t exponent, const uint64_t mod) {
-    uint64_t product{1};
-    uint64_t rolling_squares{num};
+template<typename T>
+constexpr T fast_mod_exp(const T num, T exponent, const T mod) {
+    T product{1};
+    T rolling_squares{num};
 
     /*
      * While exp is not zero:
@@ -25,28 +34,32 @@ constexpr uint64_t fast_mod_exp(const uint64_t num, uint64_t exponent, const uin
     return product;
 }
 
-constexpr uint64_t prime_modular_inverse(const uint64_t num, const uint64_t mod) {
-    return fast_mod_exp(num, mod - 2, mod);
+template<typename T>
+constexpr T prime_modular_inverse(const T num, const T mod) {
+    T exponent = mod - 2;
+    return fast_mod_exp(num, exponent, mod);
 }
 
-constexpr std::pair<uint64_t, uint64_t> el_gamal_encrypt(
-    const uint64_t generator,
-    const uint64_t modulus,
-    const uint64_t message,
-    const uint64_t ephemeral_key,
-    const uint64_t counterparty_public_key)
+template<typename T>
+constexpr std::pair<T, T> el_gamal_encrypt(
+    const T generator,
+    const T modulus,
+    const T message,
+    const T ephemeral_key,
+    const T counterparty_public_key)
 {
-    uint64_t c_1 = fast_mod_exp(generator, ephemeral_key, modulus);
-    uint64_t c_2 = message * fast_mod_exp(counterparty_public_key, ephemeral_key, modulus) % modulus;
+    T c_1 = fast_mod_exp(generator, ephemeral_key, modulus);
+    T c_2 = message * fast_mod_exp(counterparty_public_key, ephemeral_key, modulus) % modulus;
     return std::make_pair(c_1, c_2);
 }
 
-constexpr uint64_t el_gamal_decrypt(
-    const uint64_t modulus,
-    std::pair<uint64_t, uint64_t> cypher,
-    const uint64_t private_key)
+template<typename T>
+constexpr T el_gamal_decrypt(
+    const T modulus,
+    std::pair<T, T> cypher,
+    const T private_key)
 {
-    uint64_t x = fast_mod_exp(cypher.first, private_key, modulus);
+    T x = fast_mod_exp(cypher.first, private_key, modulus);
     return (cypher.second * prime_modular_inverse(x, modulus)) % modulus;
 }
 
@@ -68,6 +81,7 @@ int main() {
     std::printf("%ld to the power of %ld mod %ld is %ld\n", num, exponent, mod, product);
     std::printf("The inverse of %ld under the prime modulus %ld is %ld\n", num, prime_mod, inverse);
 
+    // El Gamal example from the textbook
     constexpr uint64_t modulus = 467;
     constexpr uint64_t generator = 2;
     constexpr uint64_t alice_private_key = 153;
@@ -79,6 +93,44 @@ int main() {
     constexpr auto cypher = el_gamal_encrypt(generator, modulus, message, ephemeral_key, alice_public_key);
     
     constexpr uint64_t decrypted_message = el_gamal_decrypt(modulus, cypher, alice_private_key);
+    assert(decrypted_message == message);
+
+    // El Gamal from zipkin
+
+    // p: 15798791135088746123550015719513096545140450581792829335173644951168904821343
+    // g: 2
+    // x: 11659842551292255865086477704221272176963931918275797345620444192196778436330
+    // c1: 7855434657566654268873728025242363007386495727068873043180877667581048349284
+    // c2: 7200831034730555987102729567301480124776715457374277384349240265842775915028
+    
+    const cpp_int pin_modulus{"15798791135088746123550015719513096545140450581792829335173644951168904821343"};
+    const cpp_int pin_generator{"2"};
+    const cpp_int pin_private_key{"11659842551292255865086477704221272176963931918275797345620444192196778436330"};
+    const cpp_int pin_public_key = fast_mod_exp(pin_generator, pin_private_key, pin_modulus);
+    const cpp_int pin_c1{"7855434657566654268873728025242363007386495727068873043180877667581048349284"};
+    const cpp_int pin_c2{"7200831034730555987102729567301480124776715457374277384349240265842775915028"};
+    const auto pin_cyphers{std::make_pair(pin_c1, pin_c2)};
+    const cpp_int pin_message(el_gamal_decrypt(pin_modulus, pin_cyphers, pin_private_key));
+    std::cout << std::hex << pin_message << std::endl;
+
+    const cpp_int tc_message{"0x3C332070696E68656164"};
+    const cpp_int big_ephemeral_key = 190000007;
+    const std::pair<cpp_int, cpp_int> tc_cypher = el_gamal_encrypt(pin_generator, pin_modulus, tc_message, big_ephemeral_key, pin_public_key);
+    std::cout << "tc c1: " << tc_cypher.first << "tc c2: " << tc_cypher.second << std::endl;
+    const cpp_int tc_decrypted_message = el_gamal_decrypt(pin_modulus, tc_cypher, pin_private_key);
+    assert(tc_decrypted_message == tc_message);
+
+    // Textbook example, but with boost bigint
+    const cpp_int big_modulus = 467;
+    const cpp_int big_generator = 2;
+    const cpp_int big_alice_private_key = 153;
+    const cpp_int big_alice_public_key = fast_mod_exp(big_generator, big_alice_private_key, big_modulus);
+    assert(alice_public_key == 224);
+
+    const cpp_int big_message = 331;
+    const std::pair<cpp_int, cpp_int> big_cypher = el_gamal_encrypt(big_generator, big_modulus, big_message, big_ephemeral_key, big_alice_public_key);
+    
+    const cpp_int big_decrypted_message = el_gamal_decrypt(big_modulus, big_cypher, big_alice_private_key);
     assert(decrypted_message == message);
 
     return 0;
